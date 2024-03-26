2a6fb8f1c329539ec9e7442ccbe1ae94d74cc760
Kuroyukimaru <oranggabut712@gmail.com>
2024-03-26 10:12:59 +0700

    Membuat Prosedure Luas persegi panjang

--- 151_FungsiSwitch/151_FungsiSwitch.cpp
+++ 151_FungsiSwitch/151_FungsiSwitch.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int luas;
+
+void prosedureLuas(int p, int l) {
+    luas = p * l;
+
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int panjang, lebar;
+    cout << "Masukkan Panjang :";
+    cin >> panjang;
+    cout << "Masukkan lebar :";
+    cin >> lebar;
+
+    prosedureLuas(panjang, lebar);
+    cout << "Luas persegi panjang dengan prosedur :" << luas << endl;
+
+
+    return 0;
 }
