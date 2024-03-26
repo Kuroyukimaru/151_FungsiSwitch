035ea92c302bbf58f2d6fe797831dc52c6b7c1fd
Kuroyukimaru <oranggabut712@gmail.com>
2024-03-26 10:38:05 +0700

    membuat fungsi sistem perhitungan

---
+++ switch/switch.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int bilangan1, bilangan2;
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
+int pengurangan(int a, int b) {
+    return a - b;
+}
+
+int perkalian(int a, int b)
+{
+    return a * b;
+}
+
+float pembagian(float a, float b) {
+    return a / b;
+}
+int main()
+{
+   
+}
